2737823c9c2162420b54d096dbb029fc4ad645d4
Yousif Wali <87617646+yousif-wali@users.noreply.github.com>
2024-01-25 00:08:35 -0600

    Update index.py

--- index.py
+++ index.py
@@ -1,7 +1,7 @@
 import os
 from random import randint
 
-for i in range(1, 30):
+for i in range(1, 100):
     d = str(i) + " days ago"
     with open('file.txt', 'a') as file:
         file.write(d)
